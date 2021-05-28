9d71d9c0fc606d1d11ac8362870956cd8b79e86f
Alexander Zerra <Alexander.Zerra@gmail.com>
2021-05-27 17:44:07 -0700

    Second Push

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { BASE_URL } from './Globals'
+
+import axios from 'axios'
 
 import Welcome from './pages/Welcome'
 import Recipes from './pages/Recipes'
@@ -7,6 +10,21 @@ import Recipes from './pages/Recipes'
 import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      recipes: [],
+      cuisines: [],
+      selectedRecipes: null,
+      recipeDetails: null
+    }
+  }
+
+  async componentDidMount() {
+    const res = await axios.get(`${BASE_URL}/api/recipes`)
+    this.setState({ recipes: res.data.results })
+  }
+
   render() {
     return (
       <div className="App">
